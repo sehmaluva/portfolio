10e19c070b0627d4f46d430195760e215af8071c
sehmaluva <machinguramalvint@gmail.com>
2025-10-02 14:49:43 +0200

    removing unnecessary files

--- react-portfolio/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Set `base` to the repository name so the built site works when served
-// from GitHub Pages at https://<username>.github.io/portfolio/
-export default defineConfig({
-  plugins: [react()],
-  base: '/portfolio/react-portfolio/',
-})
